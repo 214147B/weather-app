68a458fa72f8e894331a3f0d1ce41a27a96fd457
214147B <pereraakakk.21@uom.lk>
2024-11-29 10:50:52 +0530

    Enhance tab navigation with custom icons and update vector-icons dependency

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,44 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons';
+
+interface TabIconProps{
+  icon:any,
+  name:string,
+  focused:boolean
+}
+
+const TabIcon=({icon,name,focused}:TabIconProps )=>{
+  return(
+    <View className="items-center justify-center gap-2">
+      <Ionicons color={focused ? "white" :"#f8989"} size={24} name="home" />
+      <Text className='text-white'>{name}</Text>
+    </View>
+  )
+}
 
 const _layout = () => {
   return (
-    <Tabs>
-        <Tabs.Screen name='index'/>
+    <Tabs screenOptions={{
+      tabBarShowLabel: false,
+      tabBarInactiveTintColor: '#8f8989',
+      tabBarActiveTintColor: '#f0e9e9',
+      tabBarStyle: {
+        backgroundColor: '#000',
+        borderTopColor: '#232533',
+        borderTopWidth: 1,
+        paddingTop: 5,
+      },
+    }}>
+        <Tabs.Screen name='index' options={{title:"Home",
+        headerShown:false,
+        tabBarIcon: ({ color, focused }) => (
+          <TabIcon icon="" name="Home" focused={focused} />
+
+        )
+
+        }}/>
         <Tabs.Screen name='weather'/>
         <Tabs.Screen name='map'/>
         <Tabs.Screen name='travel'/>
