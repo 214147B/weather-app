ae7b8b14ffa90046c74b05dfebdbe935437c1781
214147B <pereraakakk.21@uom.lk>
2024-11-30 19:34:22 +0530

    Add TypeScript definitions for React Native and update background image in index screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
 import { ImageBackground, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import backgroundImage from "../../assets/images/cloud.jpg"
+import backgroundImage from "../../assets/images/claudi.jpg"
 import {LinearGradient} from "expo-linear-gradient"
 
 const index = () => {
   return (
     <View>
-      <ImageBackground className="flex-1 w-full h-[70%] bg-[#111]" source={backgroundImage} resizeMode="cover"></ImageBackground>
+      <ImageBackground className="flex-1 w-full h-full bg-[#111]" source={backgroundImage} resizeMode="stretch"></ImageBackground>
       <LinearGradient colors={["transparent","#111"]} className="absolute left-0 right-0 bottom-[30%] h-[50%] z-10"></LinearGradient>
       <Text>Weather App</Text>
     </View>
